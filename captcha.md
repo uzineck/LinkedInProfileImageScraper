51833f0697a1f322760e51f1128711e1b986c9b9
SaiGerkat <illyakrykalov@gmail.com>
2024-10-31 11:23:51 +0200

    added captcha.md

---
+++ captcha.md
@@ -0,0 +1,11 @@
+## How to avoid captcha
+You should create script that will imitate real human like experience, e.g., scrolling page,
+moving cursor, clicking with mouse, taking your time.
+
+Also, you should create such driver that will copy real web browser options and headers, 
+so that sites won't detect webdriver`s work.
+
+## How to bypass captcha
+Completely avoiding captcha may not be realistic, as companies continuously develop new methods for detecting bots.
+However, you can just solve captcha with third-party extensions, e.g., 2Captcha, ZenRows
+and others. They provide API for solving this type of challenges, though these services typically charge a fee.
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 
 import logging
+from fake_useragent import UserAgent
 from selenium import webdriver
 from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
 
 from linkedin import LinkedIn
 from utils import Utils
@@ -24,7 +26,15 @@ def get_linkedin_profile_image(web_driver: webdriver.Firefox):
     return username, profile_img_link
 
 
+def get_random_firefox_agent() -> str:
+    user_agent = UserAgent(browsers="firefox", os="windows", platforms="pc")
+    return user_agent.random
+
+
 if __name__ == "__main__":
-    driver = Firefox()
+    ua = get_random_firefox_agent()
+    options = Options()
+    options.add_argument(f'--user-agent={ua}')
+    driver = Firefox(options=options)
     image_name, image_link = get_linkedin_profile_image(web_driver=driver)
     Utils.save_image(image_name=image_name, image_url=image_link)
